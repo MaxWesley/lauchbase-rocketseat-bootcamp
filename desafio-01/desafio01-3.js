e8394d4eb7845b40bdda0a52b51e619703464005
MaxWesley <mresende@trustprev.com.br>
2021-01-30 06:11:33 -0300

    feat: checaSeUsuarioUsaCSS function

--- desafio-01/desafio01-3.js
+++ desafio-01/desafio01-3.js
@@ -3,3 +3,22 @@
 // =============================================================================
 // Funções e estruturas de repetição
 
+const usuarios = [
+    { nome: "Carlos", tecnologias: ["HTML", "CSS"] },
+    { nome: "Jasmine", tecnologias: ["JavaScript", "CSS"] },
+    { nome: "Tuane", tecnologias: ["HTML", "Node.js"] }
+]
+
+usuarios.forEach(user => {
+    console.log(`${user.nome} trabalha com ${user.tecnologias[0]}, ${user.tecnologias[1]}`)
+})
+
+function checaSeUsuarioUsaCSS(usuario) {
+    for (let i = 0; i < usuario.tecnologias.length; i++) {
+        if(usuario.tecnologias[i] === "CSS") 
+        return true;
+    }
+
+    return false
+}
+
